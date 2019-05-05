377ec58f8c5477435d0781524e34c2b4161e67c5
kanjamad bosel <kanjamadpor@gmail.com>
2019-05-04 18:45:13 -0700

    ready for heroku deploy attempt #1

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const DB_URL = 'mongodb://localhost:27017/hiking-collab';
+const DB_URL = process.env.MONGODB_URI || 'mongodb://localhost:27017/hiking-collab';
 
 mongoose.connect(DB_URL, { useNewUrlParser: true, useFindAndModify: false })
     .then(() => console.log('MongoDB connected...'))
