c4cfe1cb0f8d1bc13a77fd9de7c2e9a9897435fe
michaelpetty <michael.petty@gmail.com>
2022-10-27 14:39:16 -0700

    audit/fix packages; mongoose chng localhost to ip

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
-const DB_URL = process.env.MONGODB_URI || 'mongodb://localhost:27017/hiking-collab';
+const DB_URL = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/hiking-collab';
 
-mongoose.connect(DB_URL, { useNewUrlParser: true, useFindAndModify: false })
+mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected...'))
     .catch((err) => console.log(err));
 
